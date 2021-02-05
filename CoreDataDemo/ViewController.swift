d6fe6c966b8c6b39b34b1b2fdc109a1be1a426f5
Pavel Boltromyuk <pavel.boltromyuk99@gmail.com>
2021-02-05 11:29:22 +0300

    Initial Commit

---
+++ CoreDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Pavel Boltromyuk on 2/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
